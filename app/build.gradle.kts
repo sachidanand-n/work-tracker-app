f805de7054cfb7efdfa5b6be1673e3a9cbdc3e91
sachidanand-n <sachidanand.n@gmail.com>
2026-09-25 17:32:35 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,3 +11,7 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.7.4"))
     implementation("com.google.firebase:firebase-firestore-ktx")
 }
+id("com.android.application")
+id("com.google.gms.google-services")
+implementation(platform("com.google.firebase:firebase-bom:34.19.0"))
+implementation("com.google.firebase:firebase-analytics")
