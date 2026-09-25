51b201873ea28096e129aaadd71209581762b8d8
sachidanand-n <sachidanand.n@gmail.com>
2026-09-25 18:59:32 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/worktracker/MainActivity.kt
+++ app/src/main/java/com/example/worktracker/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
