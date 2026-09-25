7710e479779bd3f0bc965f198d037e18757ecd2f
sachidanand-n <sachidanand.n@gmail.com>
2026-09-25 17:39:30 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,3 +15,8 @@ id("com.android.application")
 id("com.google.gms.google-services")
 implementation(platform("com.google.firebase:firebase-bom:34.19.0"))
 implementation("com.google.firebase:firebase-analytics")
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
+}
